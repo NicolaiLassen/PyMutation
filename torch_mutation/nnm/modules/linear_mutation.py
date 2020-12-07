5a5fab7faaa11f60168a73025fb85b756b28322c
NicolaiLassen <nicolai.m.t.lassen@gmail.com>
2020-12-07 14:30:53 +0100

    nerual network mutation

--- torch_mutation/nnm/modules/linear_mutation.py
+++ torch_mutation/nnm/modules/linear_mutation.py
@@ -2,8 +2,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor
 
+import torch_mutation.nnm as nnm
 
-class LinearMutation(nn.Linear):
+
+class LinearMutation(nn.Linear, nnm.ModuleMutation):
     def __init__(self, in_features: int, out_features: int):
         super().__init__(in_features, out_features)
 
